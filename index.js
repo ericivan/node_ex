b901129ea8665c1d81cf49ba606e97530089a7bb
ericivan <ericivanzzl@163.com>
2018-11-02 15:53:46 +0000

    sdf

--- index.js
+++ index.js
@@ -4,12 +4,18 @@ const cookieParser=require('cookie-parser')
 
 const app=new express()
 
-app.use(cookieParser)
+app.use(cookieParser("secres-salt"))
 
 app.use("/",(req,res)=>{
-    res.cookie("test","test_cookie");
+    res.cookie('sign',"test_cookie",{signed:true});
 
-    // console.log(req.cookies)
+    res.cookie("not-sign","test_not_sign_cookie")
+    console.log(req.cookies)
+    console.log('sign cookie')
+
+    console.log(req.signedCookies)
+
+    res.clearCookie("not-sign")
     res.send("cookie has set")
 })
 
